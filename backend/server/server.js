ad19122be416cb2b2985930ba12e5f3b50573268
PeterDMP98 <peter.dmp.ca@gmail.com>
2025-07-18 16:22:42 -0500

    creacion del servidor con node express, cambio de commanj a module, creacion de env y gitignore

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,1 +1,26 @@
+// esportacion de de module express y otros modulos necesarios.
+import express from 'express'; // import express framework
+import path from 'path'; // omportar path para manejar rutas de archivos
+import { fileURLToPath} from 'url'; // importar fileURLToPath para convertir URL a ruta de archivo
+import dotenv from 'dotenv'; 
 
+dotenv.config(); // cargar variables de entorno desde un archivo .env
+const PORT = process.env.PORT || 3002; // establecer el puerto del servidor, si no está definido en .env, usar 3002
+const app = express(); // crear una instancia de express
+
+// obtener la ruta del directorio actual
+
+const __filename = fileURLToPath(import.meta.url); // obtener el nombre del archivo actual
+const __dirname = path.dirname(__filename); // obtener el nombre del directorio actual
+
+// configurar express para servir archivos estáticos desde el directorio 'public'
+app.use(express.static(path.join(__dirname, '../public'))); // servir archivos estáticos desde el directorio 'public'
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html')); // enviar el archivo index.html al cliente
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`); // mensaje en consola indicando que el servidor está corriendo
+    
+})
